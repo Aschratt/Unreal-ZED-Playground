50313788abf6f089ef54d99e3c268048df9248c8
Carsten Rudolph <18394207+Aschratt@users.noreply.github.com>
2021-03-23 13:32:26 +0100

    Import project scaffold.

---
+++ Source/ZedPlayground/ZedPlayground.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ZedPlayground.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ZedPlayground, "ZedPlayground" );
